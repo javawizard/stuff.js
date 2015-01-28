672f6e1ec02d03dd6091dac95739dd35f709f9a1
Alex Boyd <aboyd@instructure.com>
2015-01-28 01:35:56 -0700

    Array support, and a bunch of potential issues
    (Things like how to prevent a render that incidentally modifies
    a model from causing an infinite render loop)

--- stuff.js
+++ stuff.js
@@ -7,9 +7,32 @@
 // case such an object was attached to a more permanent object, but we'd
 // presumably do that anyway on account of said reattachment...
 // Also see what happens if we try to modify something while rendering.
+//
+// TODO: Another flaw: What happens if we modify stuff we're watching during
+// a render? Maybe not because we're trying to (we really shouldn't) but
+// because we, say, remove something from an array, look at it, and put it
+// back, or something like that... The fact that we really shouldn't be makes
+// me think the correct answer is to just not re-render, but then we could miss
+// out on watching newly-created objects that were attached to the model and
+// therefore _should_ be watched...
+// Possible solution: If we're told to re-render in the middle of a render, we
+// wait until the end, then manually call render() in a loop until the read
+// sets for two subsequent renders are identical, and maybe bail with an angry
+// exception if that doesn't happen after a few loops. Then update read sets
+// based on the result of the last render. There's _got_ to be a simpler
+// answer...
+// Or actually, maybe if we're asked to re-render in the middle of a render, we
+// queue that up and then check up on it after we've finished rendering, and if
+// this render produced the same read set as last render, just ignore the
+// re-render request. Need to think about that. Also need to make sure that
+// prevents two components from getting each other into mutual rendering loops,
+// which I'm pretty sure it doesn't, unless we say that that logic applies when
+// we're asked to re-render from _any_ render, not just a render of the current
+// component. Then I think it might work.
 
 Stuff = {
-  _currentReadSet = null,
+  _currentRendering: null,
+  _lastRenderingId: 0,
   
   View: {
     wrap: function(props) {
@@ -17,41 +40,43 @@ Stuff = {
       var realComponentWillUnmount = props.componentWillUnmount;
       
       props.render = function() {
-        Stuff._currentReadSet = new Set();
+        Stuff._currentRendering = {
+          id: ++Stuff._lastRenderingId,
+          stuffThatWasRead: new Set()
+        };
         
         try {
           realRender.call(this);
         } finally {
-          var oldReadSet = this.stuffWeAreWatching || new Set();
-          var newReadSet = Stuff._currentReadSet;
-          Stuff._currentReadSet = null;
+          var oldReadSet = this._stuffWeAreWatching || new Set();
+          var newReadSet = Stuff._currentRendering.stuffThatWasRead;
+          Stuff._currentRendering = null;
           
           // Attach listeners to things we read this time but not last time
           newReadSet.forEach(function(model) {
             if (!oldReadSet.has(model)) {
-              model.stuffWatchingUs = model.stuffWatchingUs || new Set();
-              model.stuffWatchingUs.add(this);
+              model._stuffWatchingUs.add(this);
             }
           }.bind(this));
           
           // Detach listeners from things we read last time but not this time
           oldReadSet.forEach(function(model) {
             if (!newReadSet.has(model)) {
-              model.stuffWatchingUs.delete(this);
+              model._stuffWatchingUs.delete(this);
             }
           });
           
           // Replace last run's read set with this one's
-          this.stuffWeAreWatching = newReadSet;
+          this._stuffWeAreWatching = newReadSet;
         }
       };
       
       props.componentWillUnmount = function() {
-        this.stuffWeAreWatching.forEach(function(model) {
-          model.delete(this);
+        this._stuffWeAreWatching.forEach(function(model) {
+          model._stuffWatchingUs.delete(this);
         }.bind(this));
         
-        this.stuffWeAreWatching = null;
+        this._stuffWeAreWatching = null;
         
         if (realComponentWillUnmount) {
           realComponentWillUnmount.call(this);
@@ -68,19 +93,28 @@ Stuff = {
   
   Model: {
     wrap: function(props) {
-      props.stuffWasRead = function() {
-        if (Stuff._currentReadSet) {
-          Stuff._currentReadSet.add(this);
+      props.weWereJustCreated = function() {
+        if (Stuff._currentRendering) {
+          this._stuffCreatedUsDuringRendering = Stuff._currentRendering.id;
+        } else {
+          this._stuffCreatedUsDuringRendering = null;
         }
-      };
+        this._stuffWatchingUs = new Set();
+      },
+      
+      props.stuffWasRead = function() {
+        // Only add ourselves to stuffThatWasRead if we were not created during
+        // the current rendering
+        if (Stuff._currentRendering && Stuff._currentRendering.id != this._stuffCreatedUsDuringRendering) {
+          Stuff._currentRendering.stuffThatWasRead.add(this);
+        }
+      },
       
       props.stuffWasWritten = function() {
-        if (this.stuffWatchingUs) {
-          this.stuffWatchingUs.forEach(function(view) {
-            view.setState(view.state);
-          }.bind(this));
-        }
-      };
+        this._stuffWatchingUs.forEach(function(view) {
+          view.setState(view.state);
+        }.bind(this));
+      },
       
       return props;
     }
@@ -111,6 +145,12 @@ Stuff = {
       // Wrap the provided constructor with one that attaches the specified
       // properties to the object
       var constructor = function() {
+        this.weWereJustCreated();
+        this._data = {};
+        
+        // TODO: Heard somewhere that props couldn't be reliably defined on a
+        // prototype. Pretty sure that's bogus now. Test that theory, then move
+        // this to the prototype.
         properties.forEach(function(prop) {
           Object.defineProperty(this, prop, {
             get: function() {
@@ -131,13 +171,11 @@ Stuff = {
       constructor.prototype = Stuff.Model.wrap(prototype);
       
       constructor.prototype.get = function(key) {
-        this._data = this._data || {};
         this.stuffWasRead();
         return this._data[key];
       };
       
       constructor.prototype.set = function(key, value) {
-        this._data = this._data || {};
         this.stuffWasWritten();
         this._data[key] = value;
       };
@@ -157,6 +195,9 @@ Stuff = {
       
       Stuff.Model.wrap(object);
       
+      object.weWereJustCreated();
+      object._data = {};
+      
       properties.forEach(function(prop) {
         Object.defineProperty(object, prop, {
           get: function() {
@@ -170,13 +211,11 @@ Stuff = {
       }.bind(this));
       
       object.get = function(key) {
-        this._data = this._data || {};
         this.stuffWasRead();
         return this._data[key];
       }
       
       object.set = function(key, value) {
-        this._data = this._data || {};
         this.stuffWasWritten();
         this._data[key] = value;
       }
@@ -192,14 +231,47 @@ Stuff = {
       // give an arbitrary object array-like behavior... which brings up an
       // interesting question: what happens if someone tries to do something
       // like Stuff.Object.wrap(Stuff.Array.wrap(foo))? Should that be allowed?
-      // I don't see why not...
+      // I don't see why not... Update: Thinking wrap() will no longer work due
+      // to the need to know when the object in question was created. This
+      // whole point might be moot.
       // Also methods like concat really ought to return new Stuff.Array.create
       // arrays so that they can also be observed.
       var object = {
-        add: function() {
-          
+        get length() {
+          this.stuffWasRead();
+          return this._items.length;
         }
       };
+      
+      Stuff.Model.wrap(object);
+      object.weWereJustCreated();
+      object._items = [];
+      
+      ['concat', 'entries', 'every', 'filter', 'find', 'findIndex', 'forEach', 'includes',
+       'indexOf', 'join', 'keys', 'lastIndexOf', 'map', 'reduce', 'reduceRight',
+       'some', 'toLocaleString', 'toString', 'values'].forEach(function(name) {
+        object[name] = function() {
+          object.stuffWasRead();
+          return object._items[name].call(object._items, arguments)
+        }
+      });
+      
+      ['copyWithin', 'fill', 'reverse', 'sort'].forEach(function(name) {
+        object[name] = function() {
+          object.stuffWasWritten();
+          object._items[name].call(object._items, arguments);
+        }
+      });
+      
+      ['pop', 'push', 'shift', 'splice', 'unshift'].forEach(function(name) {
+        object[name] = function() {
+          object.stuffWasRead();
+          object.stuffWasWritten();
+          return object._items[name].call(object._items, arguments);
+        }
+      });
+      
+      return object;
     }
   }
 }
